e3ca70c78f512c983c0e4c85279bb7398ebc400f
Chris Botaish <chbota@users.noreply.github.com>
2023-05-14 13:45:14 -0400

    support more models

--- __main__.py
+++ __main__.py
@@ -2,40 +2,47 @@ from transformers import AutoModelForCausalLM, AutoTokenizer, AutoConfig, pipeli
 from accelerate import infer_auto_device_map, init_empty_weights
 
 import sys
+import torch
 
-def initialize_model(model):
-  config = AutoConfig.from_pretrained(MODEL['model'], trust_remote_code=True, revision=MODEL['revision'])
-  print('loaded config for model {}'.format(MODEL['model']))
-  tokenizer = AutoTokenizer.from_pretrained(MODEL['model'], trust_remote_code=True, revision=MODEL['revision'])
-  print('created tokenizer for model {}'.format(MODEL['model']))
 
+def auto_generator(modelConfig):
   device_map=None
-  with init_empty_weights():
-    model = AutoModelForCausalLM.from_config(config, trust_remote_code=True)
-    print('initialized model')
-    model.tie_weights()
-    device_map = infer_auto_device_map(model)
-    print('device map', device_map)
-    model = AutoModelForCausalLM.from_pretrained(MODEL['model'], trust_remote_code=True, device_map=device_map, revision=MODEL['revision']).cuda()
-    print('loaded model to cuda')
+  def initialize_model(model):
+    config = AutoConfig.from_pretrained(MODEL['model'], trust_remote_code=True, revision=MODEL['revision'])
 
-    return (model, tokenizer)
+    print('loaded config for model {}'.format(MODEL['model']))
+    tokenizer = AutoTokenizer.from_pretrained(MODEL['model'], trust_remote_code=True, revision=MODEL['revision'])
+    print('created tokenizer for model {}'.format(MODEL['model']))
 
-def replit_generator():
-  (model, tokenizer) = initialize_model(REPLIT)
+    with init_empty_weights():
+      model = AutoModelForCausalLM.from_config(config, trust_remote_code=True)
+      print('initialized model')
+      model.tie_weights()
+      device_map = infer_auto_device_map(model)
+      print('device map', device_map)
+      model = AutoModelForCausalLM.from_pretrained(MODEL['model'], offload_folder="offload", trust_remote_code=True, device_map=device_map, revision=MODEL['revision']).half()
+      print('loaded model')
 
-  def generate(prompt):
-    x = tokenizer(prompt, return_tensors='pt').input_ids.cuda()
-    print('encoded prompt')
-    y = model.generate(x, max_length=255, do_sample=True, top_p=0.95, top_k=4, temperature=0.2, num_return_sequences=1, eos_token_id=tokenizer.eos_token_id, pad_token_id=tokenizer.eos_token_id)
-    response = tokenizer.decode(y[0], skip_special_tokens=True, clean_up_tokenization_spaces=False)
+      return (model, tokenizer)
 
-    return response
-  return generate
+  def auto_impl():
+    (model, tokenizer) = initialize_model(modelConfig)
+
+    def generate(prompt):
+      x = tokenizer(prompt, return_tensors='pt')
+      x = x.to('cuda')
+      print('encoded prompt')
+      y = model.generate(**x, max_length=255, do_sample=True, top_p=0.95, top_k=4, temperature=0.2, eos_token_id=tokenizer.eos_token_id, pad_token_id=tokenizer.eos_token_id)
+      response = tokenizer.decode(y[0], skip_special_tokens=True, clean_up_tokenization_spaces=False)
+
+      return response
+    return generate
+  
+  return auto_impl
 
 
-def make_opt_generator(model):
-  def opt_generator():
+def pipeline_generator(model):
+  def pipeline_impl():
     print('creating opt pipeline')
     generator = pipeline('text-generation', model=model, device="cuda:0")
     print('ready for prompt')
@@ -43,32 +50,49 @@ def make_opt_generator(model):
       return generator(prompt, max_length=100, batch_size=8, do_sample=True, top_p=0.95, top_k=4, temperature=0.4, num_return_sequences=1)[0]['generated_text']
     
     return generate
-  return opt_generator
+  return pipeline_impl
 
 REPLIT = {
   'model': 'replit/replit-code-v1-3b',
   'revision': 'main',
-  'make_generator': replit_generator
+  'make_generator': auto_generator('replit/replit-code-v1-3b')
 }
 OPT13 = {
   'model': 'facebook/opt-13b',
   'revision': 'main',
-  'make_generator': make_opt_generator('facebook/opt-13b')
+  'make_generator': pipeline_generator('facebook/opt-13b')
 }
 OPT6_7 = {
   'model': 'facebook/opt-6.7b',
   'revision': 'main',
-  'make_generator': make_opt_generator('facebook/opt-6.7b')
+  'make_generator': pipeline_generator('facebook/opt-6.7b')
 }
 OPT1_3 = {
   'model': 'facebook/opt-1.3b',
   'revision': 'main',
-  'make_generator': make_opt_generator('facebook/opt-1.3b')
+  'make_generator': pipeline_generator('facebook/opt-1.3b')
 }
+MPT7 = {
+  'model': 'mosaicml/mpt-7b',
+  'revision': 'main',
+  'make_generator': auto_generator('mosaicml/mpt-7b')
+}
+STARCODER = {
+  'model': 'bigcode/starcoder',
+  'revision': 'main',
+  'make_generator': auto_generator('bigcode/starcoder')
+}
+STABLELM = {
+  'model': 'StabilityAI/stablelm-tuned-alpha-3b',
+  'revision': 'main',
+  'make_generator': auto_generator('StabilityAI/stablelm-tuned-alpha-3b')
+}
+
 
 MODEL = REPLIT
 
 generate = MODEL['make_generator']()
+
 
 print(">>> ", end="", flush=True)
 for line in sys.stdin:
